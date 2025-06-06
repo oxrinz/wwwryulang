80a0bc63a514083c182f807d7b93e6d9a5eb41f0
rinz <roberts.denisovs@gmail.com>
2025-06-06 11:28:33 +0300

    front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>use ryu!</h1>
-<p>welcome. this page is wip.</p>
+<div class="w-screen h-screen flex justify-center items-center flex-col gap-8">
+    <h1 class="font-['calsans'] text-9xl">ryu!</h1>
+    <p class="font-['poppins']">welcome. this page is wip.</p>
+</div>
